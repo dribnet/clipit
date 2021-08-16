0fb0e2f4a15f06fc3d6cbfc4e4879060702113be
Tom White <tom@sixdozen.com>
2021-08-16 12:22:11 +0000

    fixed anims errors and gif building

--- clipit.py
+++ clipit.py
@@ -601,11 +601,11 @@ def make_gif(args, iter):
     if os.path.exists(gif_output):
         os.remove(gif_output)
     cmd = ['ffmpeg', '-framerate', '10', '-pattern_type', 'glob',
-           '-i', f"{args.animation_dir}/*_*.png", '-loop', '0', gif_output]
+           '-i', f"{args.animation_dir}/*.png", '-loop', '0', gif_output]
     try:
         output = subprocess.check_output(cmd)
     except subprocess.CalledProcessError as cpe:
-        output = e.output
+        output = cpe.output
         print("Ignoring non-zero exit: ", output)
 
     return gif_output
